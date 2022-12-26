38753216f6feca28f962da159251d432c33c6866
root <root@vm6.template>
2022-12-26 21:59:45 +0300

    fix bash file config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,7 @@
 #/bin/bash
 cp postgresql.conf /etc/postgresql/12/main/
 cp pg_hba.conf /etc/postgresql/12/main/
-systemctl restart postgresql
 cp -r html /var/www/
 cp apache2.conf /etc/apache2/
 systemctl restart apache2
-
+systemctl restart postgresql
