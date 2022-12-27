5d201e31bf0d4734c1863f011eb4bc5d006db443
root <root@vm6.template>
2022-12-27 08:02:35 +0300

    change apache2 to nginx

--- config.sh
+++ config.sh
@@ -3,5 +3,5 @@ cp postgresql.conf /etc/postgresql/12/main/
 cp pg_hba.conf /etc/postgresql/12/main/
 cp -r html /var/www/
 cp apache2.conf /etc/apache2/
-systemctl restart apache2
+systemctl restart nginx
 systemctl restart postgresql
